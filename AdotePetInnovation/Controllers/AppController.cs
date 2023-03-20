fa0e6df29dc5813a3f58c134bd6d136448dc6a14
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2023-03-19 21:22:36 -0300

    Fazendo a pagina do App

---
+++ AdotePetInnovation/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdotePetInnovation.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AdotePetInnovation/Controllers/SigninController.cs
+++ AdotePetInnovation/Controllers/SigninController.cs
@@ -17,19 +17,7 @@ namespace AdotePetInnovation.Controllers
             _logger = logger;
         }
         // GET: Signup
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Signup/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Signup/Create
-        public ActionResult Create()
+        public ActionResult Login()
         {
             return View();
         }
@@ -40,38 +28,11 @@ namespace AdotePetInnovation.Controllers
         public async Task<ActionResult> Login(SigninViewModel signin)
         {
             var user = await _repo.GetByEmailAsync(signin.Email);
-            if(user.Password == signin.Password){
-                return RedirectToAction("B",
-                        "FileUploadMsgView",
-                        new { FileUploadMsg = "File uploaded successfully" });
+            if(user != null && user.Password == signin.Password){
+                return RedirectToAction("Index",
+                        "App");
             }
-            return View();
-        }
-
-        // GET: Signup/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Signup/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Signup/Delete/5
-        public ActionResult Delete(int id)
-        {
+            ModelState.AddModelError("email","usuario/senha invalido");
             return View();
         }
 
