e144a5624bd736a4f714feea7009c5649981e04e
Leonardo Gigliotti <leonardosilvagigliottii@gmail.com>
2023-06-21 20:30:19 -0300

    Adicionando o GetAll, GetById, Update, Delete e o album do bootstrap
    Adicionando o GetAll, GetById, Update, Delete e o album do bootstrap

--- AdotePetInnovation/Repositories/IPublicarRepository.cs
+++ AdotePetInnovation/Repositories/IPublicarRepository.cs
@@ -4,5 +4,9 @@ namespace AdotePetInnovation.Repositories;
 
 public interface IPublicarRepository
 {
+    Task<List<Dog>> GetAllAsync();
+    Task<Dog> GetByIdAsync(string id);
     Task CreateAsync(Dog dog);
+    Task UpdateAsync(Dog dog);
+    Task DeleteAsync(string id);
 }
--- AdotePetInnovation/Repositories/PublicarRepository.cs
+++ AdotePetInnovation/Repositories/PublicarRepository.cs
@@ -15,5 +15,15 @@ public class PublicarRepository : IPublicarRepository
     public async Task CreateAsync(Dog dog) =>
         await _collection.InsertOneAsync(dog);
 
+    public async Task<List<Dog>> GetAllAsync() =>
+        await _collection.Find(_ => true).ToListAsync();
 
+    public async Task<Dog> GetByIdAsync(string id) =>
+        await _collection.Find(_ => _.Id == id).FirstOrDefaultAsync();
+
+    public async Task UpdateAsync(Dog dog) =>
+        await _collection.ReplaceOneAsync(x => x.Id == dog.Id, dog);
+
+    public async Task DeleteAsync(string id) =>
+        await _collection.DeleteOneAsync(x => x.Id == id);
 }
